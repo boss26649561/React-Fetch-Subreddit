461ea23ed7890015d6f2d05f5cef7273f795e95f
Guan-Tse Wu <guan-tse.wu@griffithuni.edu.au>
2022-04-06 14:56:08 +1000

    title changable

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [subreddit, setSubreddit] = useState("webdev");
 
   useEffect(() => {
-    fetch("https://www.reddit.com/r/webdev.json").then((res) => {
+    fetch("https://www.reddit.com/r/" + subreddit + ".json").then((res) => {
       if (res.status != 200) {
         console.log("Could not retrieve JSON");
         return;
@@ -24,7 +24,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <input type="text" className="input" value="webdev"></input>
+        <input
+          type="text"
+          className="input"
+          value={subreddit}
+          onChange={(e) => setSubreddit(e.target.value)}
+        ></input>
       </header>
       <div className="articles">
         {articles != null
